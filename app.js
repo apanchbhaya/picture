881b06c223cfae7205ba3f26ad4502548c6b79ae
apanchbhaya <57445630+apanchbhaya@users.noreply.github.com>
2019-11-25 01:24:57 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const constraints = {
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
+    cameraTrigger = document.querySelector("#camera--trigger,#camera--image")
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -14,11 +14,7 @@ function cameraStart() {
         .then(function(stream) {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
-        
     })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
 }
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
